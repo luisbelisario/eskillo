e9eff2602fb404567fc60544d437eed8b61ab3d3
luisbelisario <luisbls77@hotmail.com>
2020-09-01 20:25:58 -0300

    first commit

---
+++ src/main/java/com/eskillo/eskillo/EskilloApplication.java
@@ -0,0 +1,13 @@
+package com.eskillo.eskillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskilloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskillo/eskillo/EskilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskillo.eskillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
